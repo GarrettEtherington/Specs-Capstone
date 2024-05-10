c64d8d2399c70bcff3c7b455bb1829a8f7660d6a
Garrett Etherington <pogglesnash@gmail.com>
2024-05-10 17:00:44 -0600

    search complete

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -1,20 +1,35 @@
 import { useLocation } from "react-router";
 import styles from "./HeroPage.module.css"
+import BadProf from "../Images/BadProf.PNG"
+import BangProf from "../Images/BangProf.PNG"
+import GenosProf from "../Images/GenosProf.PNG"
+import ScottProf from "../Images/ScottProf.PNG"
 
 const Hero = () => {
     const location = useLocation()
     const data = location.state
-        
+
+    if (data.heroname === "Metal Bat") {
+        data.heropic = BadProf
+    } else if (data.heroname === "Silver Fang") {
+        data.heropic = BangProf
+    } else if (data.heroname === "Demon Cyborg") {
+        data.heropic = GenosProf
+    } else if (data.heroname = "Scott Sutherland") {
+        data.heropic = ScottProf
+    }
+
     return (
         <main>
             <div className={styles.sort}>
                 <div className={styles.leftSide}>
                     <div className={styles.portrait}>
-                        image goes here
+                        {console.log(data)}
+                        <img src={data.heropic} alt={data.heroname}/>  
                     </div>                  
                     <div className={styles.aboutMe}>
                         <h2 className={styles.bout}>About</h2>
-                        <p>Genos is a mechanical cyborg of average human height. His face and ears look like that of a normal human, made of artificial skin material, and his eyes have black sclera with yellow irises. He has spiky blond hair and his eyebrows are blond</p>
+                        <p>{data.about}</p>
                     </div>                  
                 </div>
 
@@ -23,30 +38,36 @@ const Hero = () => {
                     <div className={styles.top}>
                         <div className={styles.nameBan}>
                             <div className={styles.class}>
-                                <h1>S Class</h1>
-                                <h2>Rank 12</h2>
+                                <h1>{data.heroclass} Class</h1>
+                                <h2>Rank {data.herorank}</h2>
                             </div>
-                            <h1 className={styles.name}>Demon Cyborg</h1>
+                            <h1 className={styles.name}>{data.heroname}</h1>
                         </div>
                     </div>
 
                     <div className={styles.main}>
                         <div className={styles.style}>
                             <h2>Fighting Style</h2>
-                            <p>Genos blows shit up real good</p>
+                            <p>{data.fightingstyle}</p>
                         </div>
                         <div className={styles.slay}>
                             <h2>Monsters Slain</h2>
                             <ul>
-                                <li>Devon</li>
-                                <li>Dave</li>
-                                <li>Darick</li>
+                                <li>{data.monsterskilled[0]}</li>
+                                <li>{data.monsterskilled[1]}</li>
+                                <li>{data.monsterskilled[2]}</li>
+                                <li>{data.monsterskilled[3]}</li>
+                                <li>{data.monsterskilled[4]}</li>
+                                <li>{data.monsterskilled[5]}</li>
+                                <li>{data.monsterskilled[6]}</li>
+                                <li>{data.monsterskilled[7]}</li>
+                                <li>{data.monsterskilled[8]}</li>
+                                <li>{data.monsterskilled[9]}</li>
                             </ul>
                         </div>
                     </div>
                 </div>                  
             </div>
-            {/* <p>Name: {data.heroname}</p> */}
         </main>
     )
 }
